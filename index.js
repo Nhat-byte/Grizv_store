344cf4886907d9f589874b44f68fc0256a035575
Nhat-byte <nhattrandinh97@gmail.com>
2026-07-24 23:23:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+
+  intents: [
+
+    GatewayIntentBits.Guilds,
+
+    GatewayIntentBits.GuildMessages,
+
+    GatewayIntentBits.MessageContent
+
+  ]
+
+});
+
+client.once("ready", () => {
+
+  console.log(`Đã đăng nhập với tên ${client.user.tag}`);
+
+});
+
+client.login(process.env.TOKEN);
